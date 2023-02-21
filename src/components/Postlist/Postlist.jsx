e03fa013bf07b5797fc19aa5a23bd143de48a6cb
kieudoannam <kieunam2808@gmail.com>
2023-02-21 07:07:24 +0700

    update file

--- src/components/Postlist/Postlist.jsx
+++ src/components/Postlist/Postlist.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import "./styles.css";
 
 function Postlist(props) {
     const {getIDPost, postList} = props;
@@ -10,9 +11,13 @@ function Postlist(props) {
             {postList.map((item,id) => {
                return (
                 <li key={id} className="post-item">
-                    <span > Category: ${item.category} </span>
-                    <span onClick={()=>handleGetId(item._id)}><Link to={`http://localhost:3000/${item._id}`}><h2>{item.title}</h2></Link></span>
-                    <img src={item.images} alt={item.title}/>
+                    <img className="post-image" src={item.images} alt={item.title}/>
+                    <div className="post-content">
+                        <span className="post-category"> Category: ${item.category} </span>
+                        <span className="post-title" onClick={()=>handleGetId(item._id)}><Link to={`http://localhost:3000/${item._id}`}><h2>{item.title}</h2></Link></span>
+                        {/* <p className="post-content">{item.content}</p> */}
+                        <button onClick={()=>handleGetId(item._id)}><Link to={`http://localhost:3000/${item._id}`}>Reading</Link></button>
+                    </div>
                 </li> 
                )
             })}
